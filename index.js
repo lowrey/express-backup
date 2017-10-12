946eb10389880046315cbb8f3e6e57f05ef5ab60
lowrey <bret@lowrey.me>
2017-10-12 00:09:58 -0400

    improve error handling

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const port = process.env.PORT || 5909;
 const server = require("./server.js").app.listen(port, () => {
   console.log("This express app is listening on port:" + server.address().port);
 });
+server.timeout = 300000;
--- server.js
+++ server.js
@@ -15,16 +15,15 @@ const auth = (req, res, next) => {
   function unauthorized(res) {
     res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
     return res.sendStatus(401);
-  };
+  }
   let user = basicAuth(req);
   if (!user || !user.name || !user.pass) {
     return unauthorized(res);
-  };
+  }
   if (user.name === 'samhyde' && user.pass === 'Kratom77') {
     return next();
-  } else {
-    return unauthorized(res);
-  };
+  }
+  return unauthorized(res);
 };
 
 /*
@@ -36,10 +35,10 @@ const sendFile = (req, res, filename) => {
 */
 
 function mkdir(dir, mode) {
-  try{
+  try {
     fs.mkdirSync(dir, mode);
-  } catch(e) {
-    if(e.errno === 34) {
+  } catch (e) {
+    if (e.errno === 34) {
       mkdir(path.dirname(dir), mode);
       mkdir(dir, mode);
     }
@@ -49,46 +48,57 @@ function mkdir(dir, mode) {
 const compress = (sitename, sql, dest) => {
   const siteDir = config.defaults.path.sites + sitename + '/';
   const pubDir = siteDir + config.defaults.path.public + '/';
-    // const logDir = siteDir + config.defaults.path.logs + '/';
+  const files = [pubDir];
+  if (sql !== undefined) {
+    files.push(sql);
+  }
+  // const logDir = siteDir + config.defaults.path.logs + '/';
   return tar.c({
-    gzip: true,
-  }, [pubDir, sql]);
+    gzip: true
+  }, files);
 };
 
-const cleanup = (sitename) => {
+const cleanup = sitename => {
   console.log('cleanup:', sitename);
   const destDir = config.defaults.path.backup + sitename + '/';
   const sqlFile = destDir + sitename + '.sql';
   return fs.unlinkSync(sqlFile);
 };
 
-// TODO error handling
 app.get('/backup/:site', auth, (req, res) => {
   const sitename = req.params.site;
   const site = config.sites[sitename];
-  const destDir = config.defaults.path.backup + sitename + '/';
-  // const timestamp = Date.now();
-  const sqlFile = destDir + sitename + '.sql';
-  mkdir(destDir);
-  // res.json(args))
-  console.log('sql:', sqlFile);
-  mysqldump({
-    host: config.host,
-    user: config.user,
-    password: config.password,
-    database: site.db,
-    dest: sqlFile,
-  }, (err) => {
-    console.log('mysql done:', sqlFile);
-    if (err !== null) {
-      res.json(err);
-    } else {
+  if (site === undefined) {
+    res.status(404).send('Site not found');
+  } else {
+    const destDir = config.defaults.path.backup + sitename + '/';
+    // const timestamp = Date.now();
+    if (site.db === undefined) {
       const tarfile = destDir + sitename + '.tar.gz';
-      console.log('tar:', tarfile);
-      const task = compress(sitename, sqlFile, tarfile).pipe(res);
-      task.on('finish', () => cleanup(sitename));
+      compress(sitename).pipe(res);
+    } else {
+      const sqlFile = destDir + sitename + '.sql';
+      mkdir(destDir);
+      console.log('sql:', sqlFile);
+      mysqldump({
+        host: config.host,
+        user: config.user,
+        password: config.password,
+        database: site.db,
+        dest: sqlFile
+      }, err => {
+        console.log('mysql done:', sqlFile);
+        if (err === null) {
+          const tarfile = destDir + sitename + '.tar.gz';
+          console.log('tar:', tarfile);
+          const task = compress(sitename, sqlFile).pipe(res);
+          task.on('finish', () => cleanup(sitename));
+        } else {
+          res.json(err);
+        }
+      });
     }
-  });
+  }
 });
 
 app.get('/*', (req, res) => {
@@ -96,5 +106,5 @@ app.get('/*', (req, res) => {
 });
 
 module.exports = {
-  app: app,
+  app: app
 };
